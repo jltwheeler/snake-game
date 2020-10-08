079dfe690e9c5d4942c58f63bc576e983487d04b
jltwheeler <jltwheeler@gmail.com>
2020-10-08 23:31:17 +0100

    added flex container for main app component to center game

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
 import "./App.css";
 
 import SnakeGame from "./components/SnakeGame";
 
+const Flexbox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const App = () => {
   const WID = 400;
   const HGT = 500;
-  const BOX_SIZE = 50;
+  const BOX_SIZE = 25;
 
   return (
     <div className="App">
-      <SnakeGame width={WID} height={HGT} boxSize={BOX_SIZE} />
+      <Flexbox>
+        <SnakeGame width={WID} height={HGT} boxSize={BOX_SIZE} />
+      </Flexbox>
     </div>
   );
 };
