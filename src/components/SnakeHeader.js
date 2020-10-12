7e1971fd5fcdbcc41884f51db0f2838e721ce9be
jltwheeler <jltwheeler@gmail.com>
2020-10-12 18:27:28 +0100

    updated header styling

--- src/components/SnakeHeader.js
+++ src/components/SnakeHeader.js
@@ -9,13 +9,9 @@ const StyledHeader = styled.header`
 `;
 
 const H1 = styled.h1`
-  color: #fff;
   text-transform: uppercase;
   letter-spacing: 2px;
   font-size: 4rem;
-  background-image: linear-gradient(to right, lightblue, #357ec7);
-  -webkit-background-clip: text;
-  color: transparent;
 `;
 
 const SnakeHeader = ({ title }) => {
